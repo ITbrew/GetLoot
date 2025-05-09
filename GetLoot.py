13bc62caaec3d7659820be7e077603db6f1e4454
unknown <ITBrewLLC@gmail.com>
2025-05-09 03:00:19 -0600

    Formatted DPS with commas

--- GetLoot.py
+++ GetLoot.py
@@ -374,7 +374,7 @@ dps = calculate_dps(st.session_state.gearset)
 col1, col2 = st.columns([1, 1])
 with col1:
     st.markdown(f"## 🎯 Total DPS:")
-    st.markdown(f"### `{dps}`")
+    st.markdown(f"### `{dps:,.2f}`")
 
 with col2:
     st.markdown(f"## 🏆 High Score:")
